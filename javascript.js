55af5be0ea2ae25b42143297273b370530d5d4e5
Emanuel Serveleon <123126936+emanuel-serveleon@users.noreply.github.com>
2023-11-10 14:26:16 -0500

    Add playRound function
    Project: Rock Paper Scissors
    Course: Foundations - The Odin Project

--- javascript.js
+++ javascript.js
@@ -8,3 +8,18 @@ function getComputerChoice() {
 function getPlayerChoice() {
   return prompt("Write rock, paper or scissors");
 }
+
+function playRound(playerSelection, computerSelection) {
+  // return something like "You Lose! Paper beats Rock"
+  // make playerSelection case insensitive
+
+  playerSelection = playerSelection.toLowerCase();
+
+  if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock" || (playerSelection === "scissors" && computerSelection === "paper"))) {
+    return `You win, ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === computerSelection) {
+    return "It's a tie";
+  } else {
+    return `You lose, ${computerSelection} beats ${playerSelection}`;
+  }
+}
