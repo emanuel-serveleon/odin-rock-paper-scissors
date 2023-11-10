72a57e1669da747b0a6d42d376d7d188f7b5d898
Emanuel Serveleon <123126936+emanuel-serveleon@users.noreply.github.com>
2023-11-10 14:28:14 -0500

    Add game function
    Project: Rock Paper Scissors
    Course: Foundations - The Odin Project

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+let playerWinCount;
+let computerWinCount;
+
+game();
+
 function getComputerChoice() {
   // randomly return rock, paper or scissors
   let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -16,10 +21,29 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
   if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock" || (playerSelection === "scissors" && computerSelection === "paper"))) {
+    playerWinCount++;
     return `You win, ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === computerSelection) {
     return "It's a tie";
   } else {
+    computerWinCount++;
     return `You lose, ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+function game() {
+  //play 5 rounds to report a winner at the end with a score
+  playerWinCount = 0;
+  computerWinCount = 0;
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound(getPlayerChoice(), getComputerChoice()));
+  }
+
+  if (playerWinCount > computerWinCount) {
+    console.log("You are the winner");
+  } else if (playerWinCount < computerWinCount) {
+    console.log("You are the loser");
+  } else {
+    console.log("It's a tie");
   }
 }
